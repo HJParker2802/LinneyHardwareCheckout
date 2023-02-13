4b5dc326e2f8dd73bb385e69fd95ba8deeb88a2a
HJParker2802 <125221273+HJParker2802@users.noreply.github.com>
2023-02-13 11:06:36 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+///footer 
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " Linney Hardware Library.</p>";
+
+//nav
+document.getElementById("NavigationBar").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='Inventory.html'>Data</a></li>" +
+"<li><a href='about.html'>About</a></li>" +
+"</ul>";
+
+document.getElementById("LoginIcon").innerHTML =
+"<ul id='menu2'>" +
+"<li><a href='Login.html'>Login</a></li>" 
+"</ul>";
